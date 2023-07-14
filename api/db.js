717a35a09d24bef58516a70f3c7a047e3a52edb8
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-07-14 14:11:37 +0100

    setup env var

--- api/db.js
+++ api/db.js
@@ -1,8 +1,11 @@
 import mysql from "mysql"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "#OLA12codes@",
-    database: "blog"
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 })
